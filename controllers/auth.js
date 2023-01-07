b57138c44f5b26ccf2f32f6b39f7fc5d15c10fcc
Rafael Garcia <rafaeldk17@gmail.com>
2023-01-07 11:27:29 +0100

    Chat Backend

---
+++ controllers/auth.js
@@ -0,0 +1,117 @@
+const { response } = require('express');
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const Usuario = require('../models/usuario');
+const { generarJWT } = require('../helpers/jwt');
+
+const crearUsuario = async ( req, res = response) => { 
+
+    const { email, password } = req.body;
+
+    try{
+
+        const existeEmail = await Usuario.findOne({email});
+        if( existeEmail) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya existe'
+            });
+            
+        }
+
+        const usuario = new Usuario( req.body);
+
+        //Encrictado de contraseñas
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
+
+        //Grabar usuario en db
+        await usuario.save();
+
+        //Generar mi JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            ok:true,
+            usuario,   
+            token
+        });
+
+    }catch(error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+
+}
+
+//const login ... req, res...
+//{ ok: true, msg: 'login' }
+const login = async ( req, res = response) => { 
+    const { email, password } = req.body;
+
+    try {
+
+        const usuarioDB = await Usuario.findOne({ email });
+        if(!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Email no encontrado'
+            });
+        }
+
+        //Validar password
+        const validPassword = bcrypt.compareSync( password, usuarioDB.password );  
+        if(!validPassword){
+            return res.status(400).json({
+                ok: false,
+                msg: 'La contraseña no es valida'
+            });
+        }
+
+        //Generar el JWT
+        const token = await generarJWT(usuarioDB.id);
+         res.json({
+            ok:true,
+            usuario: usuarioDB,   
+            token
+        });
+
+
+    } catch (error) {
+        console.log(error);
+         return  res.status(500).json({
+            ok:false,
+            msg: 'Hable con el administrador'
+        });
+    }
+
+}
+
+const renewToken = async( req, res = response ) => {
+
+    const uid = req.uid;
+
+    //generar un nuevo JWT 
+    const token = await generarJWT( uid );
+
+    //Obtener el usuario por el id
+    const usuario = await Usuario.findById( uid );
+
+
+    res.json({
+        ok: true,
+        usuario,
+        token
+    })
+
+}
+
+
+module.exports = {
+    crearUsuario,
+    login,
+    renewToken
+}
